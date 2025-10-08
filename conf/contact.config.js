617a35cffbbed30bb0779a0af435847b97b8b102
ocss884 <ocss.lin@gmail.com>
2025-10-09 00:28:46 +0800

    add email contact

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -8,7 +8,7 @@ module.exports = {
       btoa(
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
-    '', // 邮箱地址 例如mail@tangly1024.com
+    'ocss.lin@gmail.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://x.com/OcssLin', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/ocss884', // 你的github个人主页 例如 https://github.com/tangly1024
